3601131dc1f5ee042a17fe00ef711c6a7fa47f46
Tricenatop <57019269+Tricenatop@users.noreply.github.com>
2023-02-04 23:24:52 -0500

    Roots new
    Added new roots

--- Assets/__Scripts/RootController.cs
+++ Assets/__Scripts/RootController.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class RootController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [Header("Public Variables")]
+    public bool stopMovement;
+
+    [Header("Adjustable Variables")]
+    [SerializeField] float timeToMove = 5f;
+    [SerializeField] LayerMask whatStopMovement;
+
+    [Header("Add before Runtime")]
+    [SerializeField] Transform movePoint;
+    [SerializeField] GameObject RootTrail;
+    [SerializeField] GameObject Tree;
+
+
     void Start()
     {
-        
+        movePoint.parent = null;
+        StartCoroutine("moveCycle");
     }
 
-    // Update is called once per frame
+    IEnumerator moveCycle()
+    {
+        yield return new WaitForSeconds(timeToMove);
+
+
+        bool findSpotToMove = false;
+        int i = 0;
+        while (!findSpotToMove)
+        {
+            int horizontal = 0;
+            int verticle = 0;
+            switch (Random.Range(0, 4))
+            {
+                case 3:
+                    horizontal = 1;
+                    break;
+                case 2:
+                    horizontal = -1;
+                    break;
+                case 1:
+                    verticle = 1;
+                    break;
+                case 0:
+                    verticle = -1;
+                    break;
+
+            }
+
+
+            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontal, verticle, 0f), .2f, whatStopMovement))
+            {
+
+                movePoint.position = movePoint.position += new Vector3(horizontal, verticle, 0f);
+                findSpotToMove = true;
+                Instantiate(RootTrail, gameObject.transform.position, Quaternion.identity, Tree.transform);
+                
+            }
+
+            i++;
+            if (i == 6)
+            {
+                findSpotToMove = true;
+            }
+        }
+
+
+
+        transform.position = movePoint.position;
+        StartCoroutine("moveCycle");
+    }
+
+
     void Update()
     {
-        
+        //transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
     }
 }
